49edd22174bfa10d785b66b61e56cfab1d314c28
MacMacky <mac21macky@gmail.com>
2019-04-29 19:48:58 +0800

    Add two new props 'operations,value' in initState

--- src/components/Rows/index.js
+++ src/components/Rows/index.js
@@ -8,15 +8,24 @@ import GridContainer from '../GridContainer';
 import Input from '../Input';
 import ImgContainer from '../ImgContainer';
 import Row5 from './Row5';
+import { calculatorLogic } from '../../helpers';
 
+
+const CHANGE_VALUE = 'CHANGE_VALUE';
 
 const initState = {
-
+  operations: [],
+  value: '0'
 }
 
-const reducer = (state = initState, action) => {
+const reducer = (state = initState, action = { type: '', payload: '' }) => {
+  switch (action.type) {
+    case CHANGE_VALUE:
+      return { ...calculatorLogic(action.payload, state) }
+    default:
+      return state;
+  }
 
-  return state;
 }
 
 
@@ -24,17 +33,20 @@ function Rows(props) {
 
   const [state, dispatch] = useReducer(reducer, initState);
 
+
+  const handleClick = (symbol) => dispatch({ type: CHANGE_VALUE, payload: symbol })
+
   return (
     <Card raised id="calc-card">
       <CardContent>
         <GridContainer>
           <ImgContainer />
-          <Input />
-          <Row1 />
-          <Row2 />
-          <Row3 />
-          <Row4 />
-          <Row5 />
+          <Input value={state.value} />
+          <Row1 handleClick={handleClick} />
+          <Row2 handleClick={handleClick} />
+          <Row3 handleClick={handleClick} />
+          <Row4 handleClick={handleClick} />
+          <Row5 handleClick={handleClick} />
         </GridContainer>
       </CardContent>
     </Card>
