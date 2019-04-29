ba967228705352657bea50a2aa5815bc7c3479e1
MacMacky <mac21macky@gmail.com>
2019-04-29 19:48:07 +0800

    Add 'helpers' folder for calculator logic

---
+++ src/helpers/index.js
@@ -0,0 +1,27 @@
+
+function calculatorLogic(symbol = '', state = { operations: [], value: '0' }) {
+  switch (symbol) {
+    case '0':
+      const newValue = parseInt(state.value) === 0 ? '0' : `${state.value}0`;
+      return { ...state, value: newValue };
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+      return { ...state, value: `${state.value === '0' ? symbol : `${state.value}${symbol}`}` };;
+    default:
+      return { ...state, value: state.value };
+  }
+}
+
+
+
+
+export {
+  calculatorLogic
+}
