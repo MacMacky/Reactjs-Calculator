c04c1d000e03a3e96a0b08296db547d2f5808104
MacMacky <mac21macky@gmail.com>
2019-04-29 19:13:03 +0800

    Add new 'Row5' comp for buttons '0' and '.'

---
+++ src/components/Rows/Row5.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import GridContainer from '../GridContainer';
+import CalculatorButton from '../CalculatorButton';
+
+
+function Row5(props) {
+  return (
+    <Grid item container spacing={8}>
+      <GridContainer size={6}>
+        <CalculatorButton
+          label="0"
+          isFullWidth
+          handleClick={props.handleClick}
+        />
+        <CalculatorButton
+          label="."
+          isFullWidth
+          handleClick={props.handleClick}
+        />
+      </GridContainer>
+    </Grid>
+  )
+};
+
+
+Row5.defaultProps = {
+  handleClick: function (e) {
+    console.log(e.target);
+  }
+}
+
+Row5.propTypes = {
+  handleClick: PropTypes.func
+}
+
+export default Row5;
