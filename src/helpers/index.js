e90cd9969b1961772f66b6ca115df575f522735b
MacMacky <mac21macky@gmail.com>
2019-04-30 07:51:50 +0800

    Add 'erase' logic in helpers/index.js

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -14,6 +14,8 @@ function calculatorLogic(symbol = '', state = { operations: [], value: '0' }) {
     case '8':
     case '9':
       return { ...state, value: `${state.value === '0' ? symbol : `${state.value}${symbol}`}` };;
+    case 'ERASE':
+      return { ...state, value: (state.value === '0' || state.value.length === 1) ? '0' : state.value.slice(0, state.value.length - 1) }
     default:
       return { ...state, value: state.value };
   }
