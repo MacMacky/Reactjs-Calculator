658392c90e13be501f1170bed069717eb4f2f3dc
MacMacky <mac21macky@gmail.com>
2019-04-30 14:48:51 +0800

    Fix bug can't concat value to current value after a 'math sign' is clicked

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,7 +1,7 @@
 import equalsLogic from "./equals";
 
-const calculatorLogic = (symbol = '', state = { operations: [], value: '0', currentSymbol: '' }) => {
-  let operations = [], value = '';
+const calculatorLogic = (symbol = '', state = { operations: [], value: '0', isMathSignClicked: false }) => {
+  let operations = [], value = '', len = 0;
   switch (symbol) {
     case '0':
       const newValue = parseInt(state.value) === 0 ? '0' : `${state.value}0`;
@@ -15,18 +15,58 @@ const calculatorLogic = (symbol = '', state = { operations: [], value: '0', curr
     case '7':
     case '8':
     case '9':
-      return { ...state, value: `${state.value === '0' || isMathSymbol(state.currentSymbol) ? symbol : `${state.value}${symbol}`}` };;
+      if (state.value === '0') {
+        value = symbol;
+      } else if (state.isMathSignClicked) {
+        value = symbol;
+      }
+      else {
+        value = `${state.value}${symbol}`;
+      }
+      return { ...state, value, isMathSignClicked: false };
     case 'ERASE':
       return { ...state, value: (state.value === '0' || state.value.length === 1) ? '0' : state.value.slice(0, state.value.length - 1) }
     case 'C':
       return { operations: [], value: '0' }
     case '+':
+      if (state.isMathSignClicked) {
+        return state;
+      }
       operations = [...state.operations, state.value, '+'];
-      return { ...state, operations, value: state.value, currentSymbol: symbol }
+      return { ...state, operations, value: state.value, isMathSignClicked: true }
+    case '-':
+      if (state.isMathSignClicked) {
+        return state;
+      }
+      operations = [...state.operations, state.value, '-'];
+      return { ...state, operations, value: state.value, isMathSignClicked: true }
+    case '/':
+      if (state.isMathSignClicked) {
+        return state;
+      }
+      operations = [...state.operations, state.value, '/'];
+      return { ...state, operations, value: state.value, isMathSignClicked: true }
+    case '*':
+      if (state.isMathSignClicked) {
+        return state;
+      }
+      operations = [...state.operations, state.value, '*'];
+      return { ...state, operations, value: state.value, isMathSignClicked: true }
     case '=':
+      len = state.operations.length;
+      if (!len || (state.value === state.operations[len - 1])) {
+        return state;
+      }
       operations = [...state.operations, state.value];
       value = equalsLogic({ operations }).toString();
-      return { operations: [value], value };
+      return { operations: [value], value, isMathSignClicked: false };
+    case '.':
+      if (state.value.includes('.')) {
+        return state;
+      } else {
+        value = `${state.value}${symbol}`;
+        return { ...state, value };
+      }
     default:
       return { ...state, value: state.value };
   }
