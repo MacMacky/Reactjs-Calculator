b761a74ba965b7467511b39507a7c1fae1c756e6
MacMacky <mac21macky@gmail.com>
2019-04-30 10:33:02 +0800

    Add 'equals' logic in 'calculatorLogic' func

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,5 +1,7 @@
+import equalsLogic from "./equals";
 
-function calculatorLogic(symbol = '', state = { operations: [], value: '0' }) {
+const calculatorLogic = (symbol = '', state = { operations: [], value: '0', currentSymbol: '' }) => {
+  let operations = [], value = '';
   switch (symbol) {
     case '0':
       const newValue = parseInt(state.value) === 0 ? '0' : `${state.value}0`;
@@ -13,11 +15,18 @@ function calculatorLogic(symbol = '', state = { operations: [], value: '0' }) {
     case '7':
     case '8':
     case '9':
-      return { ...state, value: `${state.value === '0' ? symbol : `${state.value}${symbol}`}` };;
+      return { ...state, value: `${state.value === '0' || isMathSymbol(state.currentSymbol) ? symbol : `${state.value}${symbol}`}` };;
     case 'ERASE':
       return { ...state, value: (state.value === '0' || state.value.length === 1) ? '0' : state.value.slice(0, state.value.length - 1) }
     case 'C':
-      return {  operations: [], value: '0'}
+      return { operations: [], value: '0' }
+    case '+':
+      operations = [...state.operations, state.value, '+'];
+      return { ...state, operations, value: state.value, currentSymbol: symbol }
+    case '=':
+      operations = [...state.operations, state.value];
+      value = equalsLogic({ operations }).toString();
+      return { operations: [value], value };
     default:
       return { ...state, value: state.value };
   }
@@ -25,7 +34,26 @@ function calculatorLogic(symbol = '', state = { operations: [], value: '0' }) {
 
 
 
+const isNum = (char = '') => {
+  return /[0-9]/.test(char);
+};
+
+const isMathSymbol = (char = '') => {
+  switch (char) {
+    case '+':
+    case '-':
+    case '/':
+    case '*':
+      return true;
+    default:
+      return false;
+  }
+}
+
+
 
 export {
+  isNum,
+  isMathSymbol,
   calculatorLogic
 }
