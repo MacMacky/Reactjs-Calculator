0515a650566d9e844a5334683c6b17ab457eb649
MacMacky <mac21macky@gmail.com>
2019-05-03 12:35:11 +0800

    Add testing for funcs 'computeZeroes' and 'computePercentage'

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { computeZeroes, computePercentage } from './helpers/equals';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('Test computeZeroes func', () => {
+  it('should return 100 with param 99%', () => {
+    expect(computeZeroes('99%')).toBe('100');
+  })
+  it('should return 10000 with param 99%%', () => {
+    expect(computeZeroes('99%%')).toBe('10000');
+  });
+  it('should return 1000000 with param 99%%%', () => {
+    expect(computeZeroes('99%%%')).toBe('1000000');
+  });
+  it('should return 100000000 with param 99%%%%', () => {
+    expect(computeZeroes('99%%%%')).toBe('100000000');
+  });
+});
+
+describe('Test computePercentage func', () => {
+  it('should return 0.99 with param 99%', () => {
+    expect(computePercentage('99%')).toBe(0.99);
+  });
+  it('should return 0.0099 with param 99%%', () => {
+    expect(computePercentage('99%%')).toBe(0.0099);
+  });
+  it('should return 0.000099 with param 99%%%', () => {
+    expect(computePercentage('99%%%')).toBe(0.000099);
+  });
+})
