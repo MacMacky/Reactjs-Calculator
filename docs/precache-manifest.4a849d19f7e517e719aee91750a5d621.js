f9e7157bba1b6d328d9edb478bcaa45692854583
MacMacky <mac21macky@gmail.com>
2019-06-19 11:34:47 +0800

    Add docs folder for github pages

---
+++ docs/precache-manifest.4a849d19f7e517e719aee91750a5d621.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d6c304340d411aedba95d5077df62e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2eac5b65dae8ada46b00",
+    "url": "/static/css/main.b776a85a.chunk.css"
+  },
+  {
+    "revision": "791f96dcf4258cdd1597",
+    "url": "/static/js/2.f43fa285.chunk.js"
+  },
+  {
+    "revision": "2eac5b65dae8ada46b00",
+    "url": "/static/js/main.66f1845c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "64f907e57073ae610a584c6c493cca29",
+    "url": "/static/media/logo.64f907e5.svg"
+  }
+]);
